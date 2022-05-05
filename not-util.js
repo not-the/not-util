14ad5ef989f300b882b1762379ec4f91f2d9408f
not-the <87151784+not-the@users.noreply.github.com>
2022-05-05 01:32:04 -0400

    Mouse position handler and crossorigin tags

--- not-util.js
+++ not-util.js
@@ -187,3 +187,38 @@ function capitalizeFL(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 //#endregion
+
+// Mouse position -----------------------------------------------
+
+// Mouse position handler
+// From: https://stackoverflow.com/a/7790764
+var mouseX = 0;
+var mouseY = 0;
+function trackMouse(state=true) {
+    if(state != true) { document.onmousemove = undefined; return; }
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+            (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+            (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+        
+        mouseX = event.pageX;
+        mouseY = event.pageY;
+    }
+}
